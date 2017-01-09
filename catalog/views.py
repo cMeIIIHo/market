da8e8eda3f71a7043a326a547d4947ee9535ecef
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-01-09 22:55:42 +0300

    added some URLconf, index view, index TEMPLATE

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.template import RequestContext, loader
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+
+def index(request):
+    return render_to_response("catalog/index.html")
