a92cb92b1c24379c6d9e5168951485ca097a7211
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-02-14 23:10:00 +0300

    fill DB step 2

--- parcer/filler.py
+++ parcer/filler.py
@@ -10,4 +10,9 @@ import json
 with open('vl_data.json', 'r') as f:
     data = json.load(f)
     for product in data:
+        product['Тип линз'] = product['Тип линз'] + " линзы"
         category = Category.objects.get_or_create(name=product['Тип линз'])
+
+
+        for key in product:
+            print(product[key])
