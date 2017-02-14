931f4ffa30db2f43ddb95e54cf70f5cfddb104fc
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-02-15 02:08:30 +0300

    fill DB step 4

--- parcer/filler.py
+++ parcer/filler.py
@@ -3,9 +3,9 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
 import django
 django.setup()
-
 from catalog.models import *
 import json
+
 
 with open('vl_data.json', 'r') as f:
     data = json.load(f)
@@ -20,6 +20,7 @@ with open('vl_data.json', 'r') as f:
         else:
             print(product['producer'])
             mark = Mark.objects.get_or_create(producer=product['producer'], brand='')
+
 
 
         for key in product:
