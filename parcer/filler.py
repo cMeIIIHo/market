034865b6d39992a857cd478df2bb1ed83a3e4ee3
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-02-15 19:25:08 +0300

    fill DB step 5

--- parcer/filler.py
+++ parcer/filler.py
@@ -7,6 +7,15 @@ from catalog.models import *
 import json
 
 
+def fill_product_in_db(product, category, mark):
+    Product.objects.create(
+        category=category,
+        mark=mark,
+        name=product['name'],
+        description='\n'.join([line.replace('\xa0', ' ') for line in product['description'] if len(line) > 20])
+    )
+
+
 with open('vl_data.json', 'r') as f:
     data = json.load(f)
     for num, product in enumerate(data):
@@ -20,8 +29,5 @@ with open('vl_data.json', 'r') as f:
         else:
             print(product['producer'])
             mark = Mark.objects.get_or_create(producer=product['producer'], brand='')
+        fill_product_in_db(product, category, mark)
 
-
-
-        for key in product:
-            print(key, ' = ', product[key])
