40ab5382cbeb464c83aab1741aaf78c4c8bd82c4
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-03-29 19:26:51 +0300

    correct class name in ADMIN.PY

--- catalog/admin.py
+++ catalog/admin.py
@@ -17,9 +17,9 @@ admin.site.register(Category)
 admin.site.register(Mark)
 
 
-class Option_name_Admin(admin.ModelAdmin):
+class Option_nameAdmin(admin.ModelAdmin):
     ordering = ['name']
-admin.site.register(Option_name, Option_name_Admin)
+admin.site.register(Option_name, Option_nameAdmin)
 admin.site.register(Product)
 
 
@@ -48,68 +48,3 @@ class Spec_prodAdmin(admin.ModelAdmin):
     readonly_fields = ['code']
 
 admin.site.register(Spec_prod, Spec_prodAdmin)
-
-
-
-# class Mark(models.Model):
-#     producer = models.CharField(max_length=200)
-#     brand = models. CharField(max_length=200, blank=True)
-#
-#     def __str__(self):
-#         return self.brand
-#
-#
-# class Option_name(models.Model):
-#     name = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Int_opt(models.Model):
-#     name = models.ForeignKey(Option_name)
-#     value = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Text_opt(models.Model):
-#     name = models.ForeignKey(Option_name)
-#     value = models.TextField()
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Sub_type(models.Model):
-#     name = models.CharField(max_length=100)
-#     opt_list = models.ManyToManyField(Option_name)
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Prod_type(models.Model):
-#     name = models.CharField(max_length=100)
-#     sub_type_list = models.ManyToManyField(Sub_type)
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Product(models.Model):
-#     prod_type = models.ForeignKey(Prod_type)
-#     sub_type = models.ForeignKey(Sub_type)
-#     mark = models.ForeignKey(Mark)
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Spec_prod(models.Model):
-#     product = models.ForeignKey(Product)
-#     int_opts = models.ManyToManyField(Int_opt)
-#     text_opts = models.ManyToManyField(Text_opt)
-#     code = models.IntegerField()
-#     amount = models.IntegerField()
-#     price = models.FloatField()
-#
-#     def __str__(self):
-#         return self.code
