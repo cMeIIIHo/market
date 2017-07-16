2205cb6d0e76dcdd028170e2c86710754a988352
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-07-16 23:35:45 +0300

    error, need check master branch

---
+++ loginsys/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+def registration(request):
+    if request.method == 'POST':
+        filled_form = UserCreationForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            new_user = authenticate(username=filled_form.cleaned_data['username'],
+                                    password=filled_form.cleaned_data['password'])
+            login(request, new_user)
+            return redirect('/')
+        else:
+            form = filled_form
+    else:
+        form = UserCreationForm()
+    template = 'loginsys/registration.html'
+    context = {'form': form}
+    render(request, template, context)
+
+
+
+
+
--- market/urls.py
+++ market/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('catalog.urls'))
+    url(r'^auth/', include('loginsys.urls')),
+    url(r'', include('catalog.urls')),
 ]
 
 if settings.DEBUG:
