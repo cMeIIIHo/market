23d7047cc2dc7d1603ae02306dc40428e0fd872f
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-10-06 22:10:56 +0300

    sdfsdfsdf

--- ordersys/views.py
+++ ordersys/views.py
@@ -10,33 +10,46 @@ from ordersys.forms import CartForm
 
 
 def add_sp_to_cart(request):
-    """
-    product_page ajax function
-    """
+    """ product ajax function """
     sp_id = clean_data(request.POST.get('sp_id'), int)
     sp = get_object_or_404(Spec_prod, pk=sp_id)
     quantity = clean_data(request.POST.get('sp_quantity'), int)
     user = request.user
-    if user.is_authenticated():
-        user = ProxyUser.objects.get(pk=user.pk)               # ProxyUser is made to add some methods (below)
-    else:
-        user = ProxyAnonymousUser()
-    if user.tied_cart(request):                                 # this method,
-        user.add_sp_to_cart(request, sp, quantity)             # this
-    else:
-        user.create_cart(request, sp, quantity)                # and this
-    return HttpResponse()
-
-
-def show_cart(request):
     if 'cart' in request.session:
-        order_id = request.session['cart']
-        cart = Order.objects.get(pk=order_id)
-        cart_form = CartForm(instance=cart)
-        context = {'cart_form': cart_form}
-        return render(request, 'ordersys/cart.html', context)
-    else:
         pass
+    else:
+        cart = Order().create_cart(user, sp, quantity)
+        cart.tie(request.session)
+
+
+# def add_sp_to_cart(request):
+#     """
+#     product_page ajax function
+#     """
+#     sp_id = clean_data(request.POST.get('sp_id'), int)
+#     sp = get_object_or_404(Spec_prod, pk=sp_id)
+#     quantity = clean_data(request.POST.get('sp_quantity'), int)
+#     user = request.user
+#     if user.is_authenticated():
+#         user = ProxyUser.objects.get(pk=user.pk)               # ProxyUser is made to add some methods (below)
+#     else:
+#         user = ProxyAnonymousUser()
+#     if user.tied_cart(request):                                 # this method,
+#         user.add_sp_to_cart(request, sp, quantity)             # this
+#     else:
+#         user.create_cart(request, sp, quantity)                # and this
+#     return HttpResponse()
+#
+#
+# def show_cart(request):
+#     if 'cart' in request.session:
+#         order_id = request.session['cart']
+#         cart = Order.objects.get(pk=order_id)
+#         cart_form = CartForm(instance=cart)
+#         context = {'cart_form': cart_form}
+#         return render(request, 'ordersys/cart.html', context)
+#     else:
+#         pass
 
 
 
