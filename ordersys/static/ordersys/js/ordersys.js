0dd0c88a04dfdf59885483edd95dfb0403391fe7
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-10-25 02:43:08 +0300

    - ordersys.js 'checked'attr removes from RADIOBUTTON if EPRESS DELIVERY is checked. Same goes to 'is-checked' LABEL class.

--- ordersys/static/ordersys/js/ordersys.js
+++ ordersys/static/ordersys/js/ordersys.js
@@ -8,3 +8,11 @@ function turn_off_express_delivery_checkbox() {
         checkbox.prop('checked', false);
     }
 }
+
+function turn_off_pickup_point_radio() {
+    var radio = $('input[name = "pickup_point"]:checked');
+    var radio_id = radio.prop('id');
+    var label = $('label[for = ' + radio_id + ']');
+    radio.prop('checked', false);
+    label.removeClass('is-checked');
+}
