005af35c2724fcf4ad6a6e1ded23b7579abc4823
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-10-25 22:02:21 +0300

    -ordersys.js appearance / removal of 'checked' works properly -ordersys_extras.py created fiter that adds CSS_CLASS to input element into MODELFORMS_FILEDS -cart.html appearance / removal of 'checked' works properly

--- ordersys/static/ordersys/js/ordersys.js
+++ ordersys/static/ordersys/js/ordersys.js
@@ -3,10 +3,10 @@ $( document ).ready(function() {
 });
 
 function turn_off_express_delivery_checkbox() {
-    var checkbox = $('#id_express_delivery');
-    if (checkbox.is(':checked')) {
-        checkbox.prop('checked', false);
-    }
+    var checkbox = $('#id_express_delivery:checked');
+    var label = $('label[for = id_express_delivery]');
+    checkbox.prop('checked', false);
+    label.removeClass('is-checked');
 }
 
 function turn_off_pickup_point_radio() {
