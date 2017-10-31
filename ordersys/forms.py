2d6f3dc7838b7e1a380cfe6b9d6ddee1bdfaf3a2
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-11-01 00:59:11 +0300

    form validation features, js DELIVERY_CONDITIONS logic

--- ordersys/forms.py
+++ ordersys/forms.py
@@ -3,6 +3,12 @@ from ordersys.models import Order, PickupPoint
 
 
 class OrderForm(forms.ModelForm):
+    name = forms.CharField(max_length=50, required=True,
+                           widget=forms.TextInput(attrs={'placeholder': 'example: Naumov Vitaly'}))
+    phone = forms.CharField(max_length=50, required=True,
+                            widget=forms.TextInput(attrs={'placeholder': 'example: 9110254614'}))
+    address = forms.CharField(max_length=100, required=False,
+                              widget=forms.TextInput(attrs={'placeholder': 'example: SPB, Vernosti 28'}))
     pickup_point = forms.ModelChoiceField(queryset=PickupPoint.objects.filter(is_active=True),
                                           empty_label=None,
                                           required=False,
@@ -11,6 +17,24 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['name', 'phone', 'express_delivery', 'address', 'comment', 'pickup_point']
+
+    def clean(self):
+        cleaned_data = super(forms.ModelForm, self).clean()
+        express_delivery = cleaned_data.get('express_delivery')
+        address = cleaned_data.get('address')
+        pickup_point = cleaned_data.get('pickup_point')
+        if express_delivery and pickup_point:
+            raise forms.ValidationError('U should chose either EXPRESS_DELIVERY or PICKUP_POINT')
+        if express_delivery and not address:
+            self.add_error('address', 'Please, specify delivery address')
+        if pickup_point and address:
+            self.add_error('address', "You've specified your address, but also u chose PICKUP_POINT")
+        if not express_delivery and not pickup_point:
+            raise forms.ValidationError('Please specify delivery conditions')
+
+
+
+
 
 
 # class Order(models.Model):
--- ordersys/static/ordersys/js/ordersys.js
+++ ordersys/static/ordersys/js/ordersys.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
 
+    var radio = $('input[name = "pickup_point"]:checked');
+    if (radio.prop('checked')) {
+        var address_input = $('#id_address');
+        address_input.val('').prop('disabled', true);
+    }
 });
 
 function turn_off_express_delivery_checkbox() {
@@ -7,6 +12,8 @@ function turn_off_express_delivery_checkbox() {
     var label = $('label[for = id_express_delivery]');
     checkbox.prop('checked', false);
     label.removeClass('is-checked');
+    var address_input = $('#id_address');
+    address_input.val('').prop('disabled', true);
 }
 
 function turn_off_pickup_point_radio() {
@@ -15,4 +22,6 @@ function turn_off_pickup_point_radio() {
     var label = $('label[for = ' + radio_id + ']');
     radio.prop('checked', false);
     label.removeClass('is-checked');
+    var address_input = $('#id_address');
+    address_input.val('').prop('disabled', false);
 }
