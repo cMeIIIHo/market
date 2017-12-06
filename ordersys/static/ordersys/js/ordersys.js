f5191f7b84f581047573adc5e4bcfc22493c786b
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-12-07 01:57:21 +0300

    added 'remove' button ( to ordered_item line ) it works now ( new JS script ) - it can 'display: none:' order_item line and show 'snackbar' with 'item removed' message that snackbar has 'undo' button (woks as well)

--- ordersys/static/ordersys/js/ordersys.js
+++ ordersys/static/ordersys/js/ordersys.js
@@ -5,7 +5,34 @@ $( document ).ready(function() {
         var address_input = $('#id_address');
         address_input.val('').prop('disabled', true);
     }
+
+    (function() {
+        'use strict';
+        $('[id ^= order_item_]').each(function(index, element) {
+            console.log(element);
+            var snackbarContainer = $(element).parent().find('.mdl-js-snackbar');
+            var showSnackbarButton = $(element).find('.mdl-js-button');
+            var handler = function(event) {
+            $(element).css('display', 'initial');
+            };
+            console.log(element);
+            showSnackbarButton.on('click', function() {
+                'use strict';
+                $(element).css('display', 'none');
+                var data = {
+                  message: 'Товар удален из корзины',
+                  timeout: 5000,
+                  actionHandler: handler,
+                  actionText: 'Вернуть'
+                };
+                snackbarContainer[0].MaterialSnackbar.showSnackbar(data);
+            });
+            console.log(element);
+        });
+    }());
 });
+
+
 
 function turn_off_express_delivery_checkbox() {
     var checkbox = $('#id_express_delivery:checked');
