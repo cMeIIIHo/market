dc6bbced02407776af811a89d6e0cf895617eb03
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-12-12 00:03:22 +0300

    confuration changes ( static relocation, allowed hosts )

--- market/settings.py
+++ market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+^-0$+&s9z7_4-o1h3g2q8zn9xtcm05ix8w@(emxu@op(qmi@*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['5.45.117.57']
 
 
 # Application definition
@@ -122,9 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
